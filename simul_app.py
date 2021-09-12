38a50fd554e8da3d14d0f61cd98410152a130b6a
Jorge Retamales <retamj@uw.edu>
2021-09-12 15:52:39 -0300

    working simulation. commented area chart

--- simul_app.py
+++ simul_app.py
@@ -1,213 +1,168 @@
-from numpy.lib import financial
 import streamlit as st
 import pandas as pd
 import numpy as np
 import numpy_financial as npf
 import altair as alt
-from altair import datum
 
 
-st.write("""# ¿Que me conviene, arrendar o comprar?: Un ejercicio simulado.
+
+st.write("""# ¿Cuándo me conviene arrendar vs comprar? Un ejercicio simulado.
 
 ## Introducción
 
-La respuesta es obvia dirán algunos... Si puedes, conviene siempre comprar!!! Pero esto es claramente incorrecto, dado a que tenemos
-2 alternativas financieras que compiten. Principalmente, cómo precio de venta, tasa de interés, plazo de crédito o valorización esperada
-por un lado o precio de arriendo, rentabilidad esperada de alternativa de inversión por otro lado.
-Ciertamente, es que encontraremos una combinación en donde arrendar sea más financieramente conveniente que comprar. 
-Pero cuál es ese punto. Al parecer, entonces, la pregunta parece estar mal formulado, más bien. ¿Cuando conviene, o en que condiciones comprar vs arrendar?
-El presente articúlo es un ejercicio simulado que intenta descubrir ese punto de inflexión.
+La respuesta es obvia dirán algunos... Si te alcanza para el pie, siempre conviene comprar!!! 
+Pero esto es claramente incorrecto. Debe existir una combinación de precio de venta, arriendo, tasa de interés, etc., 
+por más insólita que parezca, que haga más conveniente el arriendo. Pero cómo encontrar, esa combinación cuando las variables
+son múltiples y con formulas que para mucha gente resulta desconocida o demasiado complicada. 
+Para resolver esta dificultad, es que generé esta simulación que permite interactuar con las variables 
+que determinan cada alternativa. Idealmente ingresen los parametros con alguna situación real que les haya tocado. 
+Quién sabe, quizás las condiciones de arriendo que les estaban ofreciendo eran verdaderamente una ganga!
 
-## Escenario 
+## Simulación
+### Instrucciones generales 
+A la izquierda se encuentran los elementos interactivos para modificar la visualización que muestra cuál es la 
+alternativa más rentable. Luego de la visualización se muestran elementos importantes que son resultado de los parámetros 
+que se seleccionan y servirán para entender cómo se genera la gráfica.
+""")
+## STREAMLIT
 
-Supongamos que encontramos el departamento de nuestros sueños el cuál tienen una carácteristica especial. 
-Podemos elegir entre comprarlo o arrendarlo. Dónde el precio de venta es de 2000UF y arrendarlo 8 UF mensuales. 
-
-### Gastos
-Ahora si tuviesemos los 2000UF podríamos comprarlo de inmediato. Pero lamentablemente no es así.
-Sólo disponemos de 400UF, que ahorramos anteriormente, pero el resto tendrá que ser financiado vía crédito. 
-
-Naturalmente, el banco no nos prestará gratis los 1600 UF restantes. Para eso nos ofrece un 
-crédito tiene con una tasa de interés de 3% fija a pagar mensualmente durante 20 años. Esto, en terminos de dividendo 
-se traduce en 8.84 UF mensuales (más detalles de este cálculo en otro post). Supongamos también que el único costo complementario asociado corresponden a las contribuciones. 
-Items cómo seguros, costos notariales y cómisiones son 0. Los gastos comunes tampoco en este caso debemos considerarlo 
-en el ánalisis, dado que es un costo que debe asumir quien ocupe el departamento, ya sea propietario o arrendatario.
-
-### Valorización
-Ahora bien, eso son los parametros que definimos para cuantificar el costo del departamento, pero nos queda la otra 
-dimensión clave de nuestro análisis que es la valorización que puede tener la propiedad o del capital que logremos ahorrar
-al ser el gasto por arriendo menor a los dividendos. 
-En efecto, muchos invirtien en un bien raíz, justamente, por la esperanza que este se valorice en el tiempo. Quizás muchos han escuchado el dicho,
-'Propiedades, siempre para arriba, autos, siempre para abajo'. De igual forma, otros dirán que la propiedades no es lo único que se valoriza,
-también lo hacen instrumentos financieros como depositos a plazo, acciones o fondos mutuos. Donde la promesa, es qué, gracias al interés compuesto, 
-después de unos cuantos años nuestra inversión inicial crezca. Ambas son alternativas válidas y que tenemos que considerar en nuestro análisis.
-
-Para llevar esto a números, supongamos entonces que la propiedad se valoriza a una tasa de 4% anual, mientras que la rentabilidad de nuestro fondo 
-es de 5% anual. 
-
-
-### Resumen (hasta ahora)
-
-El dilema que entonces nos queda es el siguiente, que es más conveniente o rentable, pagar el crédito para comprar el departamento
-y al cabo de 20 años que un bien de 2000 UF sea enteramente nuestro, o arrendar, para poder aumentando nuestra capacidad de ahorro, 
-invirtiendo a su vez en un fondo mutuo, quizás, el cuál pueda rentabilizarse durante el transcurso. Para ello miremos los números
-que se muestran en el siguiente cuadro resumen para luego simular nuestras dos alternativas de inversión por separado""")
-
-df_res = pd.DataFrame({'Ítem': ['Pie Inicial/Capital Inicial FFMM','Dividendo/Arriendo Mensual', 'Valorización/Rentabilidad', 'Plazo'], 
-'Compra': ["400 UF", "8.84 UF", "4%", '20 años'], 'Arriendo': ["400", "8 UF", "5%", '20 años']})
-
-st.table(df_res)
-
-
-## Compra params
-
-costo_propiedad = st.sidebar.slider(min_value=1000,max_value= 15000, step =100, value = 2000,label="Costo Propiedad", format="%g UF")
+precio_propiedad = st.sidebar.slider(min_value=1000,max_value= 15000, step =100, value = 2000,label="Precio Propiedad", format="%g UF")
+precio_arriendo = st.sidebar.slider(min_value=3.0, max_value= 65., step =.5, value = 6.,  label="Precio Arriendo", format="%g UF")
 
 pie = st.sidebar.slider(min_value=10, max_value= 100, step = 5, 
-value = 20, label="Pie Inicial/Capital Inicial FFMM", format="%g%%")*costo_propiedad/100
+value = 20, label="% para pie o para capital inicial FFMM", format="%g%%")*precio_propiedad/100
 
 
 plazo = st.sidebar.slider(min_value=10,max_value= 30, step= 5, value = 20, format="%g años", label="Plazo")
-tasa_interes = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 1.5, label="Tasa Interés anual",format="%g%%")/100
-plusvalia = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 1.5, label="Plusvalía anual",format="%g%%")/100
+tasa_interes = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 2.8, label="Tasa Interés anual",format="%g%%")/100
+plusvalia = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 4., label="Plusvalía anual",format="%g%%")/100
 
-
-rentabilidad_ffmm = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 1.5,label="Rentabilidad FFMM", format="%g%%")/100
-
+rentabilidad_ffmm = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 4., label="Rentabilidad FFMM", format="%g%%")/100
 plazo_meses = plazo*12
 
 periodic_tasa_interes = (1+tasa_interes)**(1/12) - 1
-dividendo = -1*npf.pmt(periodic_tasa_interes , plazo_meses, costo_propiedad - pie)
-dividendo =round(1.2*dividendo, 2)
 
-costo_arriendo = st.sidebar.slider(min_value=3.0, max_value= round(1.2*dividendo, 2), step =.5, label="Costo Arriendo", format="%g UF")
+st.write("""### Resultado""")
 
-ahorro_mensual = dividendo - costo_arriendo
+mes_num = np.arange(plazo_meses)
+mes_num = mes_num + 1
+
+amortizacion_cuota_arr = npf.ppmt(tasa_interes/12, mes_num, plazo_meses, pv = -(precio_propiedad-pie))
+capital_adeudado = precio_propiedad-pie-np.cumsum(amortizacion_cuota_arr)
+interes_cuota_arr = npf.ipmt(tasa_interes/12, mes_num, plazo_meses, pv = -(precio_propiedad-pie))
+dividendos = interes_cuota_arr + amortizacion_cuota_arr
+dividendo = dividendos[0]
+
+total_pagado_credito = pie + np.cumsum(dividendos)
+costo_prepago = capital_adeudado + 1.5*interes_cuota_arr
+costo_total =  costo_prepago + total_pagado_credito
+list_valorzcn =npf.fv(plusvalia/12, mes_num, pv = -precio_propiedad, pmt =0)
+rentabilidad_br = (list_valorzcn - costo_total)
+
+ahorro_mensual = dividendo - precio_arriendo
+rentabilidad_arrendar = npf.fv(rentabilidad_ffmm/12, mes_num, pv = -pie, pmt =-ahorro_mensual)
+rentabilidad_arrendar = rentabilidad_arrendar - pie
 
 ahorro_mensual = round(ahorro_mensual, 2)
+dividendo = round(dividendo, 2)
 
 
-st.sidebar.write(f'Pie/Capital Inicial FFMM: {int(pie)} UF')
-st.sidebar.write(f'Dividendo: {dividendo} UF')
-st.sidebar.write(f'Ahorro mensual de arriendo: {ahorro_mensual} UF')
+df = pd.DataFrame({'Año':mes_num/12,  'Propiedad valorizada':list_valorzcn, 
+'Gasto crédito acumulado': total_pagado_credito, 'Amortización':  amortizacion_cuota_arr, 
+'Interés($)':interes_cuota_arr, 'Costo Arriendo':precio_arriendo, 'Arrendar': rentabilidad_arrendar, 
+'Comprar': rentabilidad_br, "Capital adeudado": capital_adeudado, 'Costo prepago': costo_prepago,
+'Costo total': costo_total})
 
-
-def gen_data(costo_propiedad, pie, plazo_meses, tasa_interes, rentabilidad_ffmm, costo_arriendo, plusvalia):
-        
-    mes_num = np.arange(plazo_meses)
-    mes_num = mes_num + 1
-
-    amortizacion_cuota_arr = npf.ppmt(tasa_interes/12, mes_num, plazo_meses, pv = -(costo_propiedad-pie))
-    capital_adeudado = costo_propiedad-pie-np.cumsum(amortizacion_cuota_arr)
-
-    interes_cuota_arr = npf.ipmt(tasa_interes/12, mes_num, plazo_meses, pv = -(costo_propiedad-pie))
-
-    dividendos = interes_cuota_arr + amortizacion_cuota_arr
-
-    total_pagado_credito = pie + np.cumsum(dividendos)
-
-    ahorro_mensual = dividendos - costo_arriendo
-
-    costo_prepago = capital_adeudado + 1.5*interes_cuota_arr
-    
-    capital_ffmm = npf.fv(rentabilidad_ffmm/12, mes_num, pv = -pie, pmt =-ahorro_mensual)
-    list_valorzcn =npf.fv(plusvalia/12, mes_num, pv = -costo_propiedad, pmt =0)
-
-    rentabilidad_br = (list_valorzcn - costo_prepago)
-    
-    df = pd.DataFrame({'Mes':mes_num,  'Propiedad valorizada':list_valorzcn, 
-    'Gasto crédito acumulado': total_pagado_credito, 'Amortización':  amortizacion_cuota_arr, 
-    'Interés($)':interes_cuota_arr, 'Costo Arriendo':costo_arriendo, 'Capital FFMM': capital_ffmm, 
-    'Rentabilidad': rentabilidad_br, "Capital adeudado": capital_adeudado, 'Costo prepago': costo_prepago})
-
-    return df
-
-df = gen_data(costo_propiedad, pie, plazo_meses, tasa_interes, rentabilidad_ffmm, costo_arriendo, plusvalia)
-
+df.to_excel("ca.xlsx")
 
 def gen_chart(df):
-    range = ['red', 'blue', 'green', 'purple']
-    domain = ['Propiedad valorizada', 'Costo prepago', "Rentabilidad", 'Capital FFMM']
+    #range = ['red', 'blue', 'green', 'purple']
+    #domain = ['Propiedad valorizada', 'Costo prepago', "Comprar", 'Arrendar']
 
     base = alt.Chart(data = df).transform_fold(
-        fold=['Propiedad valorizada', 'Costo prepago', 'Capital FFMM', 'Rentabilidad'],
+        fold=['Propiedad valorizada', 'Costo prepago', 'Arrendar', 'Comprar'],
         as_=['Tipo', 'UF']
         ).transform_calculate(
             name='"Rentabilidad"'  
         )
 
-    c = base.transform_filter(
-        alt.FieldOneOfPredicate(field='Tipo', oneOf=['Propiedad valorizada', 'Costo prepago', 'Capital FFMM'])
+    # c = base.transform_filter(
+    #     alt.FieldOneOfPredicate(field='Tipo', oneOf=['Propiedad valorizada', 'Costo prepago', 'Capital FFMM'])
+    # ).mark_line().encode(
+    # x = 'Año:Q',
+    # y = 'UF:Q')
+
+    # area = base.mark_area(opacity=0.9).encode(
+    #     x='Mes:Q',x2='Mes:Q',
+    #     y='Propiedad valorizada:Q',
+    #     y2='Costo prepago:Q',
+    # color=alt.Color('name:N', scale=alt.Scale(range=range, 
+    #     domain=domain), legend=alt.Legend(title=None))
+    # )
+    # c =c + area
+    
+    x_max= df['Año'].max()
+    y_min= np.min(df[['Comprar', 'Arrendar']].values)
+    y_max= np.max(df[['Comprar', 'Arrendar']].values)
+
+    #c.layer[0].encoding.y.title = 'UF'
+
+    range = ['green', 'purple']
+    domain = ["Comprar", 'Arrendar']
+
+    rtb = base.transform_filter(
+        alt.FieldOneOfPredicate(field='Tipo', oneOf=['Comprar', 'Arrendar'])
     ).mark_line().encode(
-    x = 'Mes:Q',
-    y = 'UF:Q',
+    x = alt.X('Año:Q', scale= alt.Scale(domain=[0, x_max], nice=False)),
+    y = alt.Y('UF:Q', scale= alt.Scale(domain=[y_min, y_max], nice=False)),
     color=alt.Color('Tipo:N', scale=alt.Scale(range=range, 
     domain=domain), legend=alt.Legend(title=None))
     )
 
-    area = base.mark_area(opacity=0.9).encode(
-        x='Mes:Q',x2='Mes:Q',
-        y='Propiedad valorizada:Q',
-        y2='Costo prepago:Q',
-    color=alt.Color('name:N', scale=alt.Scale(range=range, 
-        domain=domain), legend=alt.Legend(title=None))
-    )
-    c =c + area
+    rtb = rtb.properties(title = 'Rentabilidad de Compra vs Arriendo')
 
-    c.layer[0].encoding.y.title = 'UF'
+    range = ['green', 'purple']
+    domain = ["Comprar", 'Arrendar']
 
-    res = base.transform_filter(
-        alt.FieldOneOfPredicate(field='Tipo', oneOf=['Capital FFMM', 'Rentabilidad'])
+    rtb_c = base.transform_filter(
+        alt.FieldOneOfPredicate(field='Tipo', oneOf=['Comprar', 'Arrendar'])
     ).mark_line().encode(
-    x = 'Mes:Q',
-    y = 'UF:Q',
+    x = alt.X('Año:Q', scale= alt.Scale(domain=[0, x_max], nice=False)),
+    y = alt.Y('UF:Q', scale= alt.Scale(domain=[y_min, y_max], nice=False)),
     color=alt.Color('Tipo:N', scale=alt.Scale(range=range, 
     domain=domain), legend=alt.Legend(title=None))
     )
 
-    return alt.vconcat(c, res)
+    rtb_c = rtb_c.properties(title = 'Rentabilidad de Compra vs Arriendo')
 
 
-st.write("""## Rentabilidad de comprar: 
-
-Veamoslo de forma gráfica con los parámetros que establecimos.
-
-* La linea azul representa el monto de la propiedad de la cual somos dueños también el aúmento de su valorización.
-* La línea verde lo que hemos pagado del crédito de forma acumulada hasta el mes respectivo.
-* Finalmente, la línea negra es la diferencia entre la linea azul y verde y por consiguiente la rentabilidad neta por mes.
-""")
-
-#st.altair_chart(gen_compra_chart(pymnts_df), use_container_width=True)
-
-cmpd = gen_chart(df)
-st.write("""## Rentabilidad de Arrendar: 
-
-* 
-* 
-* 
-""")
-
-st.altair_chart(cmpd, use_container_width=True)
+    return rtb
 
 
+res = gen_chart(df) #, c
+st.altair_chart(res, use_container_width=True)
 
-#############################################################################################
-costo_propiedad = 2000
-pie = 400
-plazo_meses = 20*12
-tasa_interes = 4 / 100
-periodic_tasa_interes = (1+tasa_interes)**(1/12) - 1
-plusvalia = 0.05
+c1,c2, c3 = st.columns(3)
 
-rentabilidad_ffmm = 0.05/12
+c1.metric(label = 'Pie o Capital Inicial FFMM', value =int(pie))
+c2.metric(label = 'Dividendo', value =dividendo)
+c3.metric(label = 'Ahorro mensual de arriendo', value =ahorro_mensual)
 
+st.write("## Justificación")
+st.write("""La gráfica anterior nos muestra con buena claridad que la alternativa más rentable depende de los parámetros 
+        elegidos y el momento que las evaluemos. A partir de esto la pregunta que surge, es ¿Cómo, desde los parametros 
+        que se obtiene ese resultado?. Para ello dividamos el análisis en 2: Rentabilidad de compra y Rentabilidad de arriendo""")
 
-#df = gen_data(costo_propiedad, pie, plazo_meses, tasa_interes, rentabilidad_ffmm, costo_arriendo, plusvalia)
+st.write("### Rentabilidad de compra")
+st.write("La rentabilidad de comprar una propiedad se generó a partir de la siguiente formula")
 
-"""La rentabilidad es entonces
-
-"""
-
-#st.altair_chart(gen_chart(df), use_container_width=True)
-
-
+st.write(r"""$$RC_t= PP_t - CP_t- TP_t\\
+               RC_t= PP_t - (CP_t + TP_t)\\
+               RC_t= PP_t - CT_t\\
+                \text{ }\\
+                RC_t = \text{Rentabilidad de compra en período t}\\
+                PP_t = \text{Precio propiedad al período t}\\
+                CP_t = \text{Costo Prepago en período t}\\
+                TP_t = \text{Total pagado al período t}\\
+                CT_t = \text{Costo total prepagar en período t}\\$$""")
+                

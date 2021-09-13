ac57c6fc444ba1035cbdb3a98a44ee1cf10f1d65
Jorge Retamales <retamj@uw.edu>
2021-09-13 09:03:38 -0300

    working simulation. simplified. not comment out

--- simul_app.py
+++ simul_app.py
@@ -30,14 +30,14 @@ precio_propiedad = st.sidebar.slider(min_value=1000,max_value= 15000, step =100,
 precio_arriendo = st.sidebar.slider(min_value=3.0, max_value= 65., step =.5, value = 6.,  label="Precio Arriendo", format="%g UF")
 
 pie = st.sidebar.slider(min_value=10, max_value= 100, step = 5, 
-value = 20, label="% para pie o para capital inicial FFMM", format="%g%%")*precio_propiedad/100
+value = 20, label="Porcentaje para pie o para inversión inicial para IA", format="%g%%")*precio_propiedad/100
 
 
 plazo = st.sidebar.slider(min_value=10,max_value= 30, step= 5, value = 20, format="%g años", label="Plazo")
 tasa_interes = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 2.8, label="Tasa Interés anual",format="%g%%")/100
 plusvalia = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 4., label="Plusvalía anual",format="%g%%")/100
 
-rentabilidad_ffmm = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 4., label="Rentabilidad FFMM", format="%g%%")/100
+rentabilidad_ia_percent = st.sidebar.slider(min_value=1.0,max_value= 10.0, step= 0.05,value = 4., label="Rentabilidad esperada de instrumento alternativo (IA)", format="%g%%")/100
 plazo_meses = plazo*12
 
 periodic_tasa_interes = (1+tasa_interes)**(1/12) - 1
@@ -60,8 +60,8 @@ list_valorzcn =npf.fv(plusvalia/12, mes_num, pv = -precio_propiedad, pmt =0)
 rentabilidad_br = (list_valorzcn - costo_total)
 
 ahorro_mensual = dividendo - precio_arriendo
-rentabilidad_arrendar = npf.fv(rentabilidad_ffmm/12, mes_num, pv = -pie, pmt =-ahorro_mensual)
-rentabilidad_arrendar = rentabilidad_arrendar - pie
+rentabilidad_ia = npf.fv(rentabilidad_ia_percent/12, mes_num, pv = -pie, pmt =-ahorro_mensual)
+rentabilidad_arrendar = rentabilidad_ia - pie
 
 ahorro_mensual = round(ahorro_mensual, 2)
 dividendo = round(dividendo, 2)
@@ -71,43 +71,26 @@ df = pd.DataFrame({'Año':mes_num/12,  'Propiedad valorizada':list_valorzcn,
 'Gasto crédito acumulado': total_pagado_credito, 'Amortización':  amortizacion_cuota_arr, 
 'Interés($)':interes_cuota_arr, 'Costo Arriendo':precio_arriendo, 'Arrendar': rentabilidad_arrendar, 
 'Comprar': rentabilidad_br, "Capital adeudado": capital_adeudado, 'Costo prepago': costo_prepago,
-'Costo total': costo_total})
+'Costo total': costo_total, 'Rentabilidad IA': rentabilidad_ia, 'Inversión inicial IA': pie})
 
 df.to_excel("ca.xlsx")
 
 def gen_chart(df):
-    #range = ['red', 'blue', 'green', 'purple']
-    #domain = ['Propiedad valorizada', 'Costo prepago', "Comprar", 'Arrendar']
 
     base = alt.Chart(data = df).transform_fold(
-        fold=['Propiedad valorizada', 'Costo prepago', 'Arrendar', 'Comprar'],
+        fold=['Propiedad valorizada', 'Costo prepago', 'Arrendar', 'Comprar', 'Costo total',
+        'Rentabilidad IA', 'Inversión inicial IA'],
         as_=['Tipo', 'UF']
         ).transform_calculate(
             name='"Rentabilidad"'  
         )
-
-    # c = base.transform_filter(
-    #     alt.FieldOneOfPredicate(field='Tipo', oneOf=['Propiedad valorizada', 'Costo prepago', 'Capital FFMM'])
-    # ).mark_line().encode(
-    # x = 'Año:Q',
-    # y = 'UF:Q')
-
-    # area = base.mark_area(opacity=0.9).encode(
-    #     x='Mes:Q',x2='Mes:Q',
-    #     y='Propiedad valorizada:Q',
-    #     y2='Costo prepago:Q',
-    # color=alt.Color('name:N', scale=alt.Scale(range=range, 
-    #     domain=domain), legend=alt.Legend(title=None))
-    # )
-    # c =c + area
-    
+   
     x_max= df['Año'].max()
     y_min= np.min(df[['Comprar', 'Arrendar']].values)
     y_max= np.max(df[['Comprar', 'Arrendar']].values)
 
-    #c.layer[0].encoding.y.title = 'UF'
 
-    range = ['green', 'purple']
+    range = ['#006d2c', '#4a1486']
     domain = ["Comprar", 'Arrendar']
 
     rtb = base.transform_filter(
@@ -117,34 +100,41 @@ def gen_chart(df):
     y = alt.Y('UF:Q', scale= alt.Scale(domain=[y_min, y_max], nice=False)),
     color=alt.Color('Tipo:N', scale=alt.Scale(range=range, 
     domain=domain), legend=alt.Legend(title=None))
-    )
+    ).properties(title = 'Rentabilidad de Compra vs Arriendo')
 
-    rtb = rtb.properties(title = 'Rentabilidad de Compra vs Arriendo')
-
-    range = ['green', 'purple']
-    domain = ["Comprar", 'Arrendar']
+    range = ['#b2e2e2', '#74c476',  '#006d2c']
+    domain = ['Propiedad valorizada', 'Costo total', 'Comprar']
 
     rtb_c = base.transform_filter(
-        alt.FieldOneOfPredicate(field='Tipo', oneOf=['Comprar', 'Arrendar'])
+        alt.FieldOneOfPredicate(field='Tipo', oneOf=['Propiedad valorizada', 'Costo total', 'Comprar'])
     ).mark_line().encode(
-    x = alt.X('Año:Q', scale= alt.Scale(domain=[0, x_max], nice=False)),
-    y = alt.Y('UF:Q', scale= alt.Scale(domain=[y_min, y_max], nice=False)),
+    x = alt.X('Año:Q'),
+    y = alt.Y('UF:Q'),
     color=alt.Color('Tipo:N', scale=alt.Scale(range=range, 
     domain=domain), legend=alt.Legend(title=None))
-    )
+    ).properties(title = 'Rentabilidad de Comprar')
 
-    rtb_c = rtb_c.properties(title = 'Rentabilidad de Compra vs Arriendo')
+    range = ['#dadaeb','#807dba', '#4a1486']
+    domain = ['Rentabilidad IA', 'Inversión inicial IA', 'Arrendar']
+
+    rtb_a = base.transform_filter(
+        alt.FieldOneOfPredicate(field='Tipo', oneOf= ['Rentabilidad IA', 'Inversión inicial IA', 'Arrendar'])
+    ).mark_line().encode(
+    x = alt.X('Año:Q'),
+    y = alt.Y('UF:Q'),
+    color=alt.Color('Tipo:N', scale=alt.Scale(range=range, 
+    domain=domain), legend=alt.Legend(title=None))
+    ).properties(title = 'Rentabilidad de Arrendar')
+
+    return rtb, rtb_c, rtb_a
 
 
-    return rtb
-
-
-res = gen_chart(df) #, c
-st.altair_chart(res, use_container_width=True)
+rtb, rtb_c, rtb_a= gen_chart(df) #, c
+st.altair_chart(rtb, use_container_width=True)
 
 c1,c2, c3 = st.columns(3)
 
-c1.metric(label = 'Pie o Capital Inicial FFMM', value =int(pie))
+c1.metric(label = 'Pie o Inversión inicial en IA', value =int(pie))
 c2.metric(label = 'Dividendo', value =dividendo)
 c3.metric(label = 'Ahorro mensual de arriendo', value =ahorro_mensual)
 
@@ -153,7 +143,7 @@ st.write("""La gráfica anterior nos muestra con buena claridad que la alternati
         elegidos y el momento que las evaluemos. A partir de esto la pregunta que surge, es ¿Cómo, desde los parametros 
         que se obtiene ese resultado?. Para ello dividamos el análisis en 2: Rentabilidad de compra y Rentabilidad de arriendo""")
 
-st.write("### Rentabilidad de compra")
+st.write("### Rentabilidad de comprar")
 st.write("La rentabilidad de comprar una propiedad se generó a partir de la siguiente formula")
 
 st.write(r"""$$RC_t= PP_t - CP_t- TP_t\\
@@ -166,3 +156,24 @@ st.write(r"""$$RC_t= PP_t - CP_t- TP_t\\
                 TP_t = \text{Total pagado al período t}\\
                 CT_t = \text{Costo total prepagar en período t}\\$$""")
                 
+st.altair_chart(rtb_c, use_container_width=True)
+
+
+st.write("### Rentabilidad de arrendar")
+st.write("""La rentabilidad por arrendar a simple vista, es un poco más difícil de entender, pero no tanto.
+El punto clave es que el sólo hecho de arrendar no genera rentabilidad. Sólo esta ocurre cuando el precio 
+de arrendar sea menor al dividendo. Idealmente, además qué ese ahorro se destine a algún instrumento de inversión alternativo 
+que nos genere intereses esperados. Cómo por ejemplo un Fondo Mutuo, un depósito a plazo, acciones, bitcoin, etc. 
+
+En este caso la figura es cómo sigue, en vez de utilizar el dinero para pie de la propiedad, se inyecta en este instrumento de inversión
+alternativo (IA). A su vez, todo ahorro por concepto de pagar un menor precio de arriendo que el dividendo, 
+se depositará en el IA, con la espera que nos genere intereses a medida que pase el tiempo.""")
+
+st.write(r"""$$RA_t= RIA_t - II\\
+                \text{ }\\
+                RC_t = \text{Rentabilidad acumulada de arrendar en período t}\\
+                RIA_t = \text{Rentabilidad acumulada de instrumento de inversión alternativa al período t}\\
+                II = \text{Inversión inicial en instrumento altenativo}\\$$""")
+
+st.altair_chart(rtb_a, use_container_width=True)
+
